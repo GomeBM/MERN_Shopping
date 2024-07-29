4db4f2d1620d2e44e32491d3c4dc957406013688
GomeBM <gomebenmoshe@gmail.com>
2024-07-29 18:58:40 +0300

    added changes to categories

--- shopping_app/src/components/Product.js
+++ shopping_app/src/components/Product.js
@@ -1,3 +1,69 @@
+// import React, { useEffect } from "react";
+// import "./Product.css";
+
+// const Product = ({ product, popUpCheck }) => {
+//   useEffect(() => {
+//     // You can add any effect you need here
+//   }, [product]);
+
+//   const addToCart = async () => {
+//     const userName = window.localStorage.getItem("userName");
+//     if (!userName) {
+//       return;
+//     }
+
+//     // Ensure that product._id is used correctly
+//     const productId = product._id ? product._id.toString() : "";
+
+//     console.log("Product object:", product); // Add this line
+//     console.log("Sending request with data:", {
+//       userName,
+//       productId,
+//     });
+
+//     try {
+//       const response = await fetch("http://localhost:4000/cart/add", {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({
+//           userName,
+//           productId, // Use productId here
+//         }),
+//       });
+
+//       const data = await response.json();
+//       console.log("Response from server:", data);
+
+//       if (response.ok) {
+//         alert("Product added to cart");
+//       } else {
+//         alert(data.message);
+//       }
+//     } catch (error) {
+//       console.error("Error adding product to cart:", error);
+//       alert("An error occurred while adding the product to the cart");
+//     }
+//   };
+
+//   const handleAddToCartClick = () => {
+//     popUpCheck();
+//     addToCart();
+//   };
+
+//   return (
+//     <div className="product">
+//       <h2>{product.title}</h2>
+//       <img src={product.thumbnail} alt={product.title} loading="lazy" />
+//       <p>{product.description}</p>
+//       <p>Price: ${product.price}</p>
+//       <button onClick={handleAddToCartClick}>Add to Cart</button>
+//     </div>
+//   );
+// };
+
+// export default Product;
 import React, { useEffect } from "react";
 import "./Product.css";
 
@@ -12,10 +78,9 @@ const Product = ({ product, popUpCheck }) => {
       return;
     }
 
-    // Ensure that product._id is used correctly
     const productId = product._id ? product._id.toString() : "";
 
-    console.log("Product object:", product); // Add this line
+    console.log("Product object:", product);
     console.log("Sending request with data:", {
       userName,
       productId,
@@ -29,7 +94,7 @@ const Product = ({ product, popUpCheck }) => {
         },
         body: JSON.stringify({
           userName,
-          productId, // Use productId here
+          productId,
         }),
       });
 
@@ -37,7 +102,6 @@ const Product = ({ product, popUpCheck }) => {
       console.log("Response from server:", data);
 
       if (response.ok) {
-        alert("Product added to cart");
       } else {
         alert(data.message);
       }
@@ -48,7 +112,8 @@ const Product = ({ product, popUpCheck }) => {
   };
 
   const handleAddToCartClick = () => {
-    popUpCheck();
+    // Pass the product to popUpCheck
+    popUpCheck(product);
     addToCart();
   };
 
--- shopping_app/src/pages/shop/Shop.js
+++ shopping_app/src/pages/shop/Shop.js
@@ -1,7 +1,297 @@
+// import React, { useState, useEffect } from "react";
+// import { useCookies } from "react-cookie";
+// import ProductList from "../../components/ProductList";
+// import Popup from "../../components/Popup"; // Ensure correct path to Popup component
+// import "./Shop.css";
+
+// export const Shop = () => {
+//   const [userName, setUserName] = useState("");
+//   const [loading, setLoading] = useState(true);
+//   const [cookies, setCookies] = useCookies(["access_token"]);
+//   const [fetchedProducts, setFetchedProducts] = useState([]);
+//   const [selectedCategory, setSelectedCategory] = useState(null); // State to track selected category
+//   const [showPopup, setShowPopup] = useState(false);
+//   const [page, setPage] = useState(1);
+//   const [totalProducts, setTotalProducts] = useState(0);
+//   const limit = 8; // Number of products per page
+
+//   useEffect(() => {
+//     if (cookies.access_token) {
+//       const storedUserName = window.localStorage.getItem("userName");
+//       if (storedUserName) {
+//         setUserName(storedUserName);
+//       }
+//     }
+
+//     const fetchProducts = async () => {
+//       if (!selectedCategory) {
+//         setLoading(true);
+//         fetchAllProducts(page, limit);
+//         setLoading(false);
+//       } else {
+//         setLoading(true);
+//         fetchProductByCategory(selectedCategory);
+//         setLoading(false);
+//       }
+//     };
+
+//     fetchProducts();
+//   }, [cookies.access_token, selectedCategory, page]); // useEffect runs whenever cookies.access_token, selectedCategory, or page changes
+
+//   const handleCategoryClick = (category) => {
+//     setSelectedCategory(category === selectedCategory ? null : category);
+//     setPage(1); // Reset to first page when category changes
+//   };
+
+//   const handleAddToCart = () => {
+//     const userName = window.localStorage.getItem("userName");
+//     if (!userName) {
+//       setShowPopup(true);
+//     } else {
+//       // Add the product to the cart logic here
+//     }
+//   };
+
+//   const totalPages = Math.ceil(totalProducts / limit);
+
+//   const fetchAllProducts = async (page = 1, limit = 20) => {
+//     try {
+//       const response = await fetch(
+//         `http://localhost:4000/productsDB?page=${page}&limit=${limit}`
+//       );
+//       const data = await response.json();
+
+//       // Check if the data format is correct
+//       if (!data || !Array.isArray(data.products)) {
+//         throw new Error("Unexpected response format");
+//       }
+
+//       console.log("Fetched products data:", data); // Log the data received from API
+//       setFetchedProducts(data.products);
+//       setTotalProducts(data.totalProducts);
+//     } catch (error) {
+//       console.error("Error fetching all products:", error);
+//     }
+//   };
+
+//   const fetchProductByCategory = async (category) => {
+//     try {
+//       const response = await fetch(`http://localhost:4000/${category}?page=${page}&limit=${limit}`);
+//       const data = await response.json();
+//       setFetchedProducts(data.products);
+//       setTotalProducts(data.totalProducts);
+//     } catch (error) {
+//       console.error(`Error fetching products for category ${category}:`, error);
+//     }
+//   };
+
+//   return (
+//     <div className="shop">
+//       <h1>The Gambashop!</h1>
+//       <div className="shop-title">
+//         {loading ? (
+//           <h1>Loading</h1>
+//         ) : cookies.access_token ? (
+//           <h1>Hello {userName}</h1>
+//         ) : (
+//           <h3>Please log in to view your cart</h3>
+//         )}
+//       </div>
+//       <div className="filter-options">
+//         <button
+//           className={`filter-button ${
+//             selectedCategory === "smartphones" ? "active" : ""
+//           }`}
+//           onClick={() => handleCategoryClick("smartphones")}
+//         >
+//           Smartphones
+//         </button>
+//         <button
+//           className={`filter-button ${
+//             selectedCategory === "beauty" ? "active" : ""
+//           }`}
+//           onClick={() => handleCategoryClick("beauty")}
+//         >
+//           Beauty
+//         </button>
+//         <button
+//           className={`filter-button ${
+//             selectedCategory === "groceries" ? "active" : ""
+//           }`}
+//           onClick={() => handleCategoryClick("groceries")}
+//         >
+//           Groceries
+//         </button>
+//         <button
+//           className={`filter-button ${
+//             selectedCategory === "fragrances" ? "active" : ""
+//           }`}
+//           onClick={() => handleCategoryClick("fragrances")}
+//         >
+//           Fragrances
+//         </button>
+//         <button
+//           className={`filter-button ${
+//             selectedCategory === "furniture" ? "active" : ""
+//           }`}
+//           onClick={() => handleCategoryClick("furniture")}
+//         >
+//           Furniture
+//         </button>
+//         <button
+//           className={`filter-button ${
+//             selectedCategory === "home-decoration" ? "active" : ""
+//           }`}
+//           onClick={() => handleCategoryClick("home-decoration")}
+//         >
+//           Home Decoration
+//         </button>
+//         <button
+//           className={`filter-button ${
+//             selectedCategory === "kitchen-accessories" ? "active" : ""
+//           }`}
+//           onClick={() => handleCategoryClick("kitchen-accessories")}
+//         >
+//           Kitchen Accessories
+//         </button>
+//         <button
+//           className={`filter-button ${
+//             selectedCategory === "laptops" ? "active" : ""
+//           }`}
+//           onClick={() => handleCategoryClick("laptops")}
+//         >
+//           Laptops
+//         </button>
+//         <button
+//           className={`filter-button ${
+//             selectedCategory === "mens-shirts" ? "active" : ""
+//           }`}
+//           onClick={() => handleCategoryClick("mens-shirts")}
+//         >
+//           Mens Shirts
+//         </button>
+//         <button
+//           className={`filter-button ${
+//             selectedCategory === "mens-shoes" ? "active" : ""
+//           }`}
+//           onClick={() => handleCategoryClick("mens-shoes")}
+//         >
+//           Mens Shoes
+//         </button>
+//         <button
+//           className={`filter-button ${
+//             selectedCategory === "mens-watches" ? "active" : ""
+//           }`}
+//           onClick={() => handleCategoryClick("mens-watches")}
+//         >
+//           Mens Watches
+//         </button>
+//         <button
+//           className={`filter-button ${
+//             selectedCategory === "mobile-accessories" ? "active" : ""
+//           }`}
+//           onClick={() => handleCategoryClick("mobile-accessories")}
+//         >
+//           Mobile Accessories
+//         </button>
+//         <button
+//           className={`filter-button ${
+//             selectedCategory === "motorcycle" ? "active" : ""
+//           }`}
+//           onClick={() => handleCategoryClick("motorcycle")}
+//         >
+//           Motorcycle
+//         </button>
+//         <button
+//           className={`filter-button ${
+//             selectedCategory === "skin-care" ? "active" : ""
+//           }`}
+//           onClick={() => handleCategoryClick("skin-care")}
+//         >
+//           Skin Care
+//         </button>
+//         <button
+//           className={`filter-button ${
+//             selectedCategory === "sports-accessories" ? "active" : ""
+//           }`}
+//           onClick={() => handleCategoryClick("sports-accessories")}
+//         >
+//           Sports Accessories
+//         </button>
+//         <button
+//           className={`filter-button ${
+//             selectedCategory === "sunglasses" ? "active" : ""
+//           }`}
+//           onClick={() => handleCategoryClick("sunglasses")}
+//         >
+//           Sunglasses
+//         </button>
+//         <button
+//           className={`filter-button ${
+//             selectedCategory === "tablets" ? "active" : ""
+//           }`}
+//           onClick={() => handleCategoryClick("tablets")}
+//         >
+//           Tablets
+//         </button>
+//         <button
+//           className={`filter-button ${
+//             selectedCategory === "vehicle" ? "active" : ""
+//           }`}
+//           onClick={() => handleCategoryClick("vehicle")}
+//         >
+//           Vehicle
+//         </button>
+//         <button
+//           className={`filter-button ${
+//             selectedCategory === "womens-watches" ? "active" : ""
+//           }`}
+//           onClick={() => handleCategoryClick("womens-watches")}
+//         >
+//           Womens Watches
+//         </button>
+//         <button
+//           className={`filter-button ${
+//             selectedCategory === "womens-jewellery" ? "active" : ""
+//           }`}
+//           onClick={() => handleCategoryClick("womens-jewellery")}
+//         >
+//           Womens Jewellery
+//         </button>
+//       </div>
+//       <ProductList products={fetchedProducts} popUpCheck={handleAddToCart} />
+//       {totalProducts > limit && (
+//         <div className="pagination">
+//           <button onClick={() => setPage(page - 1)} disabled={page === 1}>
+//             Previous
+//           </button>
+//           <span>
+//             Page {page} of {totalPages}
+//           </span>
+//           <button
+//             onClick={() => setPage(page + 1)}
+//             disabled={page === totalPages}
+//           >
+//             Next
+//           </button>
+//         </div>
+//       )}
+//       {showPopup && (
+//         <Popup
+//           message="Please log in to add items to your cart"
+//           onClose={() => setShowPopup(false)}
+//         />
+//       )}
+//     </div>
+//   );
+// };
+
+// export default Shop;
+
 import React, { useState, useEffect } from "react";
 import { useCookies } from "react-cookie";
 import ProductList from "../../components/ProductList";
-import Popup from "../../components/Popup"; // Ensure correct path to Popup component
+import Popup from "../../components/Popup";
 import "./Shop.css";
 
 export const Shop = () => {
@@ -9,11 +299,11 @@ export const Shop = () => {
   const [loading, setLoading] = useState(true);
   const [cookies, setCookies] = useCookies(["access_token"]);
   const [fetchedProducts, setFetchedProducts] = useState([]);
-  const [selectedCategory, setSelectedCategory] = useState(null); // State to track selected category
-  const [showPopup, setShowPopup] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [showPopup, setShowPopup] = useState({ show: false, message: "" });
   const [page, setPage] = useState(1);
   const [totalProducts, setTotalProducts] = useState(0);
-  const limit = 20; // Number of products per page
+  const limit = 8;
 
   useEffect(() => {
     if (cookies.access_token) {
@@ -26,29 +316,39 @@ export const Shop = () => {
     const fetchProducts = async () => {
       if (!selectedCategory) {
         setLoading(true);
-        const data = await fetchAllProducts(page, limit);
-        setFetchedProducts(data.products);
-        setTotalProducts(data.totalProducts);
+        fetchAllProducts(page, limit);
         setLoading(false);
       } else {
+        setLoading(true);
         fetchProductByCategory(selectedCategory);
+        setLoading(false);
       }
     };
 
     fetchProducts();
-  }, [cookies.access_token, selectedCategory, page]); // useEffect runs whenever cookies.access_token, selectedCategory, or page changes
+  }, [cookies.access_token, selectedCategory, page]);
 
   const handleCategoryClick = (category) => {
     setSelectedCategory(category === selectedCategory ? null : category);
-    setPage(1); // Reset to first page when category changes
+    setPage(1);
   };
 
-  const handleAddToCart = () => {
+  const handleAddToCart = (product) => {
     const userName = window.localStorage.getItem("userName");
     if (!userName) {
-      setShowPopup(true);
+      setShowPopup({
+        show: true,
+        message: "Please log in to add items to your cart",
+      });
     } else {
       // Add the product to the cart logic here
+      // Example: Add product to cart in the backend or local storage
+
+      // Show confirmation popup
+      setShowPopup({
+        show: true,
+        message: `${product.title} has been added to your cart`,
+      });
     }
   };
 
@@ -61,25 +361,25 @@ export const Shop = () => {
       );
       const data = await response.json();
 
-      // Check if the data format is correct
       if (!data || !Array.isArray(data.products)) {
         throw new Error("Unexpected response format");
       }
 
-      console.log("Fetched products data:", data); // Log the data received from API
-      return data;
+      setFetchedProducts(data.products);
+      setTotalProducts(data.totalProducts);
     } catch (error) {
       console.error("Error fetching all products:", error);
-      // Return default values to avoid breaking the app
-      return { products: [], totalProducts: 0 };
     }
   };
 
   const fetchProductByCategory = async (category) => {
     try {
-      const response = await fetch(`http://localhost:4000/${category}`);
+      const response = await fetch(
+        `http://localhost:4000/${category}?page=${page}&limit=${limit}`
+      );
       const data = await response.json();
       setFetchedProducts(data.products);
+      setTotalProducts(data.totalProducts);
     } catch (error) {
       console.error(`Error fetching products for category ${category}:`, error);
     }
@@ -260,7 +560,7 @@ export const Shop = () => {
         </button>
       </div>
       <ProductList products={fetchedProducts} popUpCheck={handleAddToCart} />
-      {selectedCategory === null && (
+      {totalProducts > limit && (
         <div className="pagination">
           <button onClick={() => setPage(page - 1)} disabled={page === 1}>
             Previous
@@ -276,10 +576,10 @@ export const Shop = () => {
           </button>
         </div>
       )}
-      {showPopup && (
+      {showPopup.show && (
         <Popup
-          message="Please log in to add items to your cart"
-          onClose={() => setShowPopup(false)}
+          message={showPopup.message}
+          onClose={() => setShowPopup({ show: false, message: "" })}
         />
       )}
     </div>
