22f7f358031b80f63292a3cd090c56eb068b1380
GomeBM <gomebenmoshe@gmail.com>
2024-08-21 17:51:09 +0300

    changed to bcryptjs

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,7 @@
 const userModel = require("../models/userModel");
 const productModel = require("../models/productModel");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 require("dotenv").config();
 
 //GET a single user
